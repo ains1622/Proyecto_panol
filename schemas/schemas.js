3f975193926924a84093146d9b1e4ebdc27ff85d
Juan Jara <131215762+ains1622@users.noreply.github.com>
2024-11-20 18:51:36 -0300

    Mutations agregados

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -377,13 +377,84 @@ const typeDefs = gql`
         getCarrerasByEscuela(escuela: String!): [Carrera]
         getCarrerasByNombre(nombre: String!): [Carrera]
         getCarrerasBySede(sede: String!): [Carrera]
-        
+
     }
     type Mutation {
+        """ MUTATIONS DE OBJETOS """
+
         """  Mutations de Equipo """ 
         addEquipo(input: EquipoInput): Equipo
         updateEquipo(id: ID!, input: EquipoInput): Equipo
         deleteEquipo(id: ID!): Alert
+        """ Mutations de Herramienta """
+        addHerramienta(input: HerramientaInput): Herramienta
+        updateHerramienta(id: ID!, input: HerramientaInput): Herramienta
+        deleteHerramienta(id: ID!): Alert
+        """ Mutations de Material """
+        addMaterial(input: MaterialInput): Material
+        updateMaterial(id: ID!, input: MaterialInput): Material
+        deleteMaterial(id: ID!): Alert
+
+        """ MUTATIONS GEOGRAFICOS """
+
+        """ Mutations de Sede """
+        addSede(input: SedeInput): Sede
+        updateSede(id: ID!, input: SedeInput): Sede
+        deleteSede(id: ID!): Alert
+        """ Mutations de Comuna """
+        addComuna(input: ComunaInput): Comuna
+        updateComuna(id: ID!, input: ComunaInput): Comuna
+        deleteComuna(id: ID!): Alert
+        """ Mutations de Ciudad """
+        addCiudad(input: CiudadInput): Ciudad
+        updateCiudad(id: ID!, input: CiudadInput): Ciudad
+        deleteCiudad(id: ID!): Alert
+        """ Mutations de Region """
+        addRegion(input: RegionInput): Region
+        updateRegion(id: ID!, input: RegionInput): Region
+        deleteRegion(id: ID!): Alert
+
+        """ MUTATIONS DE PRESTAMOS """
+
+        """ Mutations de Prestamo Equipo Estudiante """
+        addPrestamoEquipoE(input: PrestamoEquipoEInput): PrestamoEquipoE
+        updatePrestamoEquipoE(id: ID!, input: PrestamoEquipoEInput): PrestamoEquipoE
+        deletePrestamoEquipoE(id: ID!): Alert
+        """ Mutations de Prestamo Equipo Docente """
+        addPrestamoEquipoD(input: PrestamoEquipoDInput): PrestamoEquipoD
+        updatePrestamoEquipoD(id: ID!, input: PrestamoEquipoDInput): PrestamoEquipoD
+        deletePrestamoEquipoD(id: ID!): Alert
+        """ Mutations de Prestamo Herramienta Estudiante """
+        addPrestamoHerramientaE(input: PrestamoHerramientaEInput): PrestamoHerramientaE
+        updatePrestamoHerramientaE(id: ID!, input: PrestamoHerramientaEInput): PrestamoHerramientaE
+        deletePrestamoHerramientaE(id: ID!): Alert
+        """ Mutations de Prestamo Herramienta Docente """
+        addPrestamoHerramientaD(input: PrestamoHerramientaDInput): PrestamoHerramientaD
+        updatePrestamoHerramientaD(id: ID!, input: PrestamoHerramientaDInput): PrestamoHerramientaD
+        deletePrestamoHerramientaD(id: ID!): Alert
+        """ Mutations de Prestamo Material Estudiante """
+        addPrestamoMaterialE(input: PrestamoMaterialEInput): PrestamoMaterialE
+        updatePrestamoMaterialE(id: ID!, input: PrestamoMaterialEInput): PrestamoMaterialE
+        deletePrestamoMaterialE(id: ID!): Alert
+        """ Mutations de Prestamo Material Docente """
+        addPrestamoMaterialD(input: PrestamoMaterialDInput): PrestamoMaterialD
+        updatePrestamoMaterialD(id: ID!, input: PrestamoMaterialDInput): PrestamoMaterialD
+        deletePrestamoMaterialD(id: ID!): Alert
+
+        """ MUTATIONS DE USUARIOS """
+        """ Mutations de Estudiante """
+        addEstudiante(input: EstudianteInput): Estudiante
+        updateEstudiante(id: ID!, input: EstudianteInput): Estudiante
+        deleteEstudiante(id: ID!): Alert
+        """ Mutations de Docente """
+        addDocente(input: DocenteInput): Docente
+        updateDocente(id: ID!, input: DocenteInput): Docente
+        deleteDocente(id: ID!): Alert
+
+        """ Mutations de Carrera """
+        addCarrera(input: CarreraInput): Carrera
+        updateCarrera(id: ID!, input: CarreraInput): Carrera
+        deleteCarrera(id: ID!): Alert
     }
 
 `;
